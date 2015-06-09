ca78deef5b7bf64722eebcb6620ca2cc06ca8ed4
lotrik <lotrik@mail.ru>
2015-06-09 10:30:51 +0300

    DZ3_Android_spring_2015

--- src/com/itstep/pastukhov/Strings/Main.java
+++ src/com/itstep/pastukhov/Strings/Main.java
@@ -10,33 +10,42 @@ public class Main {
     public static void main(String[] args) {
         task1_ReplaceChar();
         task2_NumberInAlphabet();
+        task3_ReplaceRaToRo();
+        task4_ReplaceSubstring();
+        task5_InsertSubstring();
+        task6_InsertWord();
     }
 
+    /* Заменяем каждый 3-й символ в слове знаком - */
     public static void task1_ReplaceChar (){
         String ourString = "Each Java source file contains a single public class or interface";
         String[] splitString = ourString.split(" ");
         StringBuilder sb;
         String ourNewString = "";
-        for (int i=0;i<splitString.length;i++){
+
+        for (int i = 0; i < splitString.length; i++){
             if (splitString[i].length() >= 3) {
                 sb = new StringBuilder(splitString[i]);
                 sb.setCharAt(2, '-');
                 ourNewString += sb.toString().concat(" ");
             }
         }
+
         System.out.println("Task 1");
         System.out.println(ourNewString);
+        System.out.println();
     }
 
     public static void task2_NumberInAlphabet (){
+        int num;
         String ourString = "В русском тексте каждую букву заменить ее порядковым номером в алфавите";
         char ch[] = new char[ourString.length()];
-        ourString.getChars(0,ourString.length(),ch,0);
+        ourString.getChars(0, ourString.length(), ch, 0);
         String ourStringFinal = new String();
         String numberChar = "";
         String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        int num;
-        for (int i=0;i<ourString.length();i++) {
+
+        for (int i = 0; i < ourString.length(); i++) {
             if (Character.isAlphabetic(ch[i])) {
                 ourStringFinal += ch[i];
                 ourStringFinal += "  ";
@@ -54,8 +63,71 @@ public class Main {
             }
 
         }
+
         System.out.println("Task 2");
         System.out.println(ourStringFinal);
         System.out.println(numberChar);
+        System.out.println();
+    }
+
+    public static void task3_ReplaceRaToRo () {
+        String ourString = "Mira Miro Miri Miru Miry Mire Mir Ra";
+        StringBuilder sb = new StringBuilder(ourString);
+
+        System.out.println("Task 3");
+        System.out.println(sb.toString().toLowerCase().replaceAll("ra", "ro"));
+        System.out.println();
+    }
+
+    /* Заменяем в тексте слова длинной 5 символов на подстроку*/
+    public static void task4_ReplaceSubstring () {
+        String ourString = "В тексте слова заданной длины заменить указанной подстрокой, длина которой может не " +
+                            "совпадать с длиной слова";
+        String[] splitString = ourString.split(" ");
+        String ourFinalString = new String();
+
+        for (int i = 0; i < splitString.length; i++) {
+            if (splitString[i].length() == 5) {
+                splitString[i] = ourString.substring(30,43);
+                ourFinalString += splitString[i].concat(" ");
+            } else {
+                ourFinalString += splitString[i].concat(" ");
+            }
+        }
+
+        System.out.println("Task 4");
+        System.out.println(ourFinalString);
+        System.out.println();
+    }
+
+    /* После 10-го символа в тексте вставляем подстроку*/
+    public static void task5_InsertSubstring () {
+        String ourString = "В тексте после k-го символа вставить заданную подстроку.";
+        StringBuilder sb = new StringBuilder(ourString);
+
+        sb.insert(10, ourString.substring(20, 30));
+
+        System.out.println("Task 5");
+        System.out.println(sb);
+        System.out.println();
+    }
+
+    /* Вставляем после каждого слова заканчивающегося подстрокой "ой" слово "*****" */
+    public static void task6_InsertWord () {
+        String ourString = "После каждого слова текста, заканчивающегося заданной подстрокой вставить указанное слово";
+        String[] splitString = ourString.split(" ");
+        String ourFinalString = new String();
+
+        for (int i = 0; i < splitString.length; i++) {
+            if (splitString[i].endsWith("ой")) {
+                ourFinalString += splitString[i].concat(" ***** ");
+            } else {
+                ourFinalString += splitString[i].concat(" ");
+            }
+        }
+
+        System.out.println("Task 6");
+        System.out.println(ourFinalString);
+        System.out.println();
     }
 }
